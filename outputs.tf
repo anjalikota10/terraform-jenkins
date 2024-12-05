4ed60754c05fd73a8325972b6c84339b6be1bad5
anjalikota10 <anjalikota161@gmail.com>
2024-12-05 17:23:50 +0530

    eks

--- outputs.tf
+++ outputs.tf
@@ -10,14 +10,3 @@ output "private_subnet_ids" {
   value = module.private_subnets.subnet_ids
 }
 
-output "eks_cluster_id" {
-  value = module.eks.eks_cluster_id
-}
-
-output "eks_cluster_endpoint" {
-  value = module.eks.eks_cluster_endpoint
-}
-
-output "eks_cluster_security_group_id" {
-  value = module.eks.eks_cluster_security_group_id
-}
